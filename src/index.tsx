16c5552e42f5f5718b2efc1b1064078095e52bbc
Arash Kadkhodaei <slasharash@protonmail.com>
2020-05-13 11:24:25 +0430

    use CRA and TypeScript in the project

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Products Grid</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
