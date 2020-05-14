66903a9bd7b0341759b497272d8b4c15760db345
Arash Kadkhodaei <slasharash@protonmail.com>
2020-05-14 14:13:13 +0430

    format date for each face to show related date

--- src/components/FaceItem/FaceItem.tsx
+++ src/components/FaceItem/FaceItem.tsx
@@ -1,6 +1,7 @@
 import React, { memo } from "react";
 
 import Face from "types/Face";
+import { formatDate } from "utils/date";
 import { Container, Date, FaceIcon, Head } from "./styles";
 import FaceInfo from "components/FaceInfo";
 
@@ -12,7 +13,7 @@ const FaceItem: React.FC<Props> = ({ face }) => {
   return (
     <Container>
       <Head>
-        <Date>3 days ago</Date>
+        <Date>{formatDate(face.date)}</Date>
         <FaceIcon>{face.face}</FaceIcon>
       </Head>
       <FaceInfo price={face.price} size={face.size} />
---
+++ src/utils/date.ts
@@ -0,0 +1,39 @@
+export const relativeDate = (date: string) => {
+  const second = 1000;
+  const minute = 60 * second;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const currentDate = new Date();
+  const targetDate = new Date(date);
+  const elapsedTime = currentDate.valueOf() - targetDate.valueOf();
+
+  const formats = [
+    { value: day, singular: "a day ago", plural: "days ago" },
+    { value: hour, singular: "an hour ago", plural: "hours ago" },
+    { value: minute, singular: "a minute ago", plural: "minutes ago" },
+    { value: second, singular: "a second ago", plural: "seconds ago" },
+  ];
+
+  for (const format of formats) {
+    if (format.value < elapsedTime) {
+      if (format.value * 2 > elapsedTime) {
+        return format.singular;
+      }
+      return `${Math.floor(elapsedTime / format.value)} ${format.plural}`;
+    }
+  }
+  return new Error("Invalid date");
+};
+
+export const formatDate = (date: string) => {
+  const week = 604800000;
+  const currentDate = new Date();
+  const targetDate = new Date(date);
+  const elapsedTime = currentDate.valueOf() - targetDate.valueOf();
+
+  if (elapsedTime > week) {
+    return targetDate.toDateString();
+  }
+  return relativeDate(date);
+};
