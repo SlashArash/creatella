1d36d42a2412f4c140555d0dbef4a2f864b76455
Arash Kadkhodaei <slasharash@protonmail.com>
2020-05-15 11:06:41 +0430

    load more data on scroll

--- src/containers/MainBody/MainBody.tsx
+++ src/containers/MainBody/MainBody.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useRef } from "react";
 
 import Face from "types/Face";
 import { host, requestOptions } from "utils/constants";
@@ -12,6 +12,7 @@ const MainBody = () => {
   const [page, setPage] = useState<number>(1);
   const [faces, setFaces] = useState<Face[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isAllDataFetched, setIsAllDataFetched] = useState<boolean>(false);
 
   const loadFaces = async () => {
     const productsURL = `${host}products?_sort=${sortBy}&_page=${page}&_limit=15`;
@@ -20,12 +21,38 @@ const MainBody = () => {
     setFaces([...faces, ...newFaces]);
     setPage(page + 1);
     setIsLoading(false);
+    if (newFaces.length === 0) {
+      setIsAllDataFetched(true);
+    }
   };
+
   const loadFacesCallback = useCallback(loadFaces, []);
 
   useEffect(() => {
     loadFacesCallback();
   }, [loadFacesCallback]);
+
+  useEffect(() => {
+    const loadOnScroll = async () => {
+      const scrollTop =
+        (document.documentElement && document.documentElement.scrollTop) ||
+        document.body.scrollTop;
+      const scrollHeight =
+        (document.documentElement && document.documentElement.scrollHeight) ||
+        document.body.scrollHeight;
+      const clientHeight =
+        (document.documentElement && document.documentElement.clientHeight) ||
+        window.innerHeight;
+      const scrolledToBottom =
+        Math.ceil(scrollTop + clientHeight) >= scrollHeight;
+
+      if (scrolledToBottom && !isLoading && !isAllDataFetched) {
+        await loadFaces();
+      }
+    };
+    window.addEventListener("scroll", loadOnScroll);
+    return () => window.removeEventListener("scroll", loadOnScroll);
+  }, [loadFaces, isLoading, isAllDataFetched]);
 
   return (
     <Wrapper>
